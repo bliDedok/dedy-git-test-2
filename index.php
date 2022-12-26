6701332682148acec3a6965d2cd3d20b50e1f142
bliDedok <imadededywanditya@gmail.com>
2022-12-26 15:01:04 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque harum accusantium deserunt culpa nostrum praesentium eveniet quisquam, magnam a error possimus dolore. Veniam ipsum odit aperiam, voluptas est sequi soluta?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
